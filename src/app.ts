6785389b7d806a13b471ddc8480f726e337c0eba
rayhan108 <raihanshorker56@gmail.com>
2025-11-07 16:04:57 +0600

    add new url

--- src/app.ts
+++ src/app.ts
@@ -31,6 +31,7 @@ app.use(
       'http://10.10.20.13:5000',
       'http://10.10.20.13:3000',
       'http://localhost:5175',
+      'http://localhost:5173',
       'http://localhost:5174',
       'https://smith-williums-web.vercel.app',
       'https://desert-safari-dashboard.vercel.app',
